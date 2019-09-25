e8de6228d4dcdc8ff0c9cce707315f699a48a42a
helabenkhalfallah <hela@upperskills.com>
2019-09-25 21:27:54 +0200

    [Feature] ui kit module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import RKButton from './components/Button/RKButton';
+import Icons from './components/icons/icons';
 
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+export {
+  RKButton,
+  Icons,
+};
